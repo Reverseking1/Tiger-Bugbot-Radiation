2784b1315de15d2ef2c0b977441c865f91e2e90a
Reverseking1 <127144970+Reverseking1@users.noreply.github.com>
2024-08-18 18:49:16 +0000

    Update classicconf.js

--- classicconf.js
+++ classicconf.js
@@ -26,11 +26,11 @@ global.ig = 'kipkoech' // ubah aja
 global.email = 'samuelcircute@gmail.com' //serah
 global.region = 'kenya' // serah
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'DRAGON' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Reverse  King ❤️💰 ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['254746764468'] // ubah aja pake nomor lu
+global.owner = ['233257514504'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
-global.botname = '⫹TIGER-RADIATION⫺' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.botname = 'Reverse  King ❤️💰' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.packname = '⫹⸙⧉TIGER-BUGBOT-RADIATION⧉⸙⫺' // ubah aja ini nama sticker
 global.author = '༻࿇𝙰𝚁𝙻𝙾_𝙳𝚁𝙰𝙶𝙾𝙽❖༺' // ubah aja ini nama sticker
 global.prefa = ['','!','.',',','🐤','🗿']
